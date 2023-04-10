68dac8993a51e20150397e7c8b750ef88bb3e599
Duy Thanh <lothanhduy2003@gmail.com>
2023-04-10 22:51:32 +0700

    update more routes

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,16 +1,39 @@
 import {
     getLoginScreen,
     getSignUpScreen,
-    getHomePage
+    getHomeScreen,
+    getCategoryScreen,
+    getCategoriesParent,
+    getCategoriesChild,
+    handleSignUp,
+    handleLogin,
+    handleLogOut,
+    handleAddCategoryParent,
+    handleAddCategoryChild,
+    getProductScreen,
+    handleAddProduct
 } from '../controllers/web'
+import { requireLoggedIn, upload } from '../functions/middleware'
 import express from 'express'
+import cookieParser from 'cookie-parser'
 
 let route = express.Router()
 
 const initWebRoutes = (app) => {
     app.get('/login', getLoginScreen)
     app.get('/sign-up', getSignUpScreen)
-    app.get('/', getHomePage)
+    app.get('/', cookieParser(), requireLoggedIn, getHomeScreen)
+    app.get('/category', getCategoryScreen)
+    app.get('/categories-parent', getCategoriesParent)
+    app.get('/categories-child', getCategoriesChild)
+    app.get('/product', getProductScreen)
+
+    app.post('/handle-sign-up', handleSignUp)
+    app.post('/handle-login', cookieParser(), handleLogin)
+    app.post('/handle-log-out', cookieParser(), handleLogOut)
+    app.post('/handle-add-category-parent', handleAddCategoryParent)
+    app.post('/handle-add-category-child', handleAddCategoryChild)
+    app.post('/handle-add-product', upload.single('image'), handleAddProduct)
 
     return app.use('/login', route)
 }
