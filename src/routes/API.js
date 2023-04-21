237ad4c8d925499abeb1c29bcc7c927f62e91f2f
Duy Thanh <lothanhduy2003@gmail.com>
2023-04-21 14:51:25 +0700

    create routes login, gethome, search foods

--- src/routes/API.js
+++ src/routes/API.js
@@ -5,8 +5,11 @@ const route = express.Router()
 
 const initAPIRoute = (app) => {
     route.get('/get-all-user', api.getAllUser)
+    route.get('/get-all-product', api.getHomeProduct)
 
     route.post('/handle-sign-up-user', api.handleSignUpUser)
+    route.post('/handle-login-user', api.handleLoginUser)
+    route.post('/handle-search-product-by-name', api.handleSearchFoodByName)
 
     return app.use('/api/v1', route)
 }
