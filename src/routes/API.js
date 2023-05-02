7b0979639ab07c0d53fa77c05548789391fc801d
Duy Thanh <lothanhduy2003@gmail.com>
2023-05-02 07:59:30 +0700

    make routes getRice, finduser, update phone, email

--- src/routes/API.js
+++ src/routes/API.js
@@ -6,10 +6,14 @@ const route = express.Router()
 const initAPIRoute = (app) => {
     route.get('/get-all-user', api.getAllUser)
     route.get('/get-all-product', api.getHomeProduct)
+    route.get('/get-all-rice', api.getAllRice)
+    route.get('/find-user-by-id/:id', api.findUserById)
 
     route.post('/handle-sign-up-user', api.handleSignUpUser)
     route.post('/handle-login-user', api.handleLoginUser)
     route.post('/handle-search-product-by-name', api.handleSearchFoodByName)
+    route.post('/handle-update-phone-number-user', api.handleUpdatePhoneNumber)
+    route.post('/handle-update-email-user', api.handleUpdateEmail)
 
     return app.use('/api/v1', route)
 }
